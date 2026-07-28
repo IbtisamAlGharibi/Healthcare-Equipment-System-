37bf03549b4835a5cef63a77941839c77ea19a84
wejdan al-subhi <alwejdan97@gmail.com>
2026-07-28 08:50:03 +0400

    create maintenance class and add attributes

--- HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/Entities/Maintenance.java
+++ HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/Entities/Maintenance.java
@@ -1,4 +1,28 @@
 package com.example.HealthcareEquipmentSystem.Entities;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
 public class Maintenance {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private LocalDate maintenanceDate;
+    private String description;
+    private Boolean isActive;
+    private String status;
+
+    @ManyToOne
+    @JoinColumn(name = "tech_id")
+    private MaintenanceTechnician technician;
+
+    /*@ManyToOne
+    @JoinColumn(name = "equip_id")
+    private Equipment equipment;*/
 }
--- HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/Entities/MaintenanceTechnician.java
+++ HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/Entities/MaintenanceTechnician.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 
+import java.util.List;
+
 @Entity
 @Data
 public class MaintenanceTechnician {
