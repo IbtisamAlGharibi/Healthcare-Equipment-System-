39318372d370ae4a566efcceae56e202d688eb21
wejdan al-subhi <alwejdan97@gmail.com>
2026-07-28 10:20:21 +0400

    create TechnicianService

--- HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/Services/TechnicianService.java
+++ HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/Services/TechnicianService.java
@@ -1,4 +1,15 @@
 package com.example.HealthcareEquipmentSystem.Services;
 
+import com.example.HealthcareEquipmentSystem.Repositories.TechnicianRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TechnicianService {
+    private TechnicianRepository technicianRepository;
+    @Autowired
+    public TechnicianService(TechnicianRepository technicianRepository) {
+        this.technicianRepository = technicianRepository;
+    }
+
 }
