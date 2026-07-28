e2df1258f29334fdce88b1f69f2dc2ef5c65f594
Ibtisam <ibtisamalgharibi02@gmail.com>
2026-07-28 13:47:34 +0400

    update in the Entites

--- HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/Entities/Maintenance.java
+++ HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/Entities/Maintenance.java
@@ -1,13 +1,17 @@
 package com.example.HealthcareEquipmentSystem.Entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 
 import java.time.LocalDate;
 
 @Entity
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Maintenance {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
