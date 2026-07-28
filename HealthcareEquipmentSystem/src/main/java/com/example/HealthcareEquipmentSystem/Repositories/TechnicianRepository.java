3a20ac510b5883725a23645e30ccd7fda2be99dd
wejdan al-subhi <alwejdan97@gmail.com>
2026-07-28 09:36:58 +0400

    adding methods in repository

--- HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/Repositories/TechnicianRepository.java
+++ HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/Repositories/TechnicianRepository.java
@@ -1,4 +1,17 @@
 package com.example.HealthcareEquipmentSystem.Repositories;
 
-public class TechnicianRepository {
+import com.example.HealthcareEquipmentSystem.Entities.MaintenanceTechnician;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface TechnicianRepository extends JpaRepository<MaintenanceTechnician, Integer> {
+    @Query("SELECT S FROM MaintenanceTechnicia M WHERE M.isActive=true")
+    List<MaintenanceTechnician> findAllByIsActive();
+    @Query("SELECT S FROM MaintenanceTechnicia M WHERE M.specialization= :specialization")
+    List<MaintenanceTechnician> findBySpecialization(@Param("specialization") String specialization);
+    @Query("SELECT S FROM MaintenanceTechnicia M WHERE M.specialization= :specialization AND M.isActive=true")
+    List<MaintenanceTechnician> findBySpecializationAndIsActive(@Param("specialization") String specialization, Boolean isActive);
 }
