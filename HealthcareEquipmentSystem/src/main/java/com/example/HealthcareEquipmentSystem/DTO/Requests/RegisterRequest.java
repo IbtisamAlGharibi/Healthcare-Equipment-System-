470aff6b97a9fa770c6683b9a8ac8f868d5449c4
Ibtisam Hamdan Al-Gharibi <ibtisamalgharibi02@gmail.com>
2026-07-29 10:22:17 +0400

    User roles (#7)
    * User class is created
    
    * RegisterRequest is created
    
    * LoginRequest is created
    
    * UserRepository is created
    
    * CustomUserDetailsService is created
    
    * JwtUtil is created
    
    * JwtAuthFilter is created
    
    * SecurityConfig is created
    
    * AuthController is created

---
+++ HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/DTO/Requests/RegisterRequest.java
@@ -0,0 +1,16 @@
+package com.example.HealthcareEquipmentSystem.DTO.Requests;
+
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+    private String username;
+    private String password;
+    private String role; // ADMIN, LAB_STAFF, or TECHNICIAN
+}
---
+++ HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/Entities/User.java
@@ -0,0 +1,30 @@
+package com.example.HealthcareEquipmentSystem.Entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.jspecify.annotations.Nullable;
+
+@Entity
+@AllArgsConstructor
+
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true, nullable = false)
+    private String username;
+    @Column(nullable = false)
+    private String password;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    public User(String username, @Nullable String encode, Role role) {
+        this.username = username;
+        this.password = encode;
+        this.role = role;
+    }
+}
