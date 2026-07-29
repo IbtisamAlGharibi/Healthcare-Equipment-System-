2428c51c01576617519a3871cc76486836717f35
Ibtisam Hamdan Al-Gharibi <ibtisamalgharibi02@gmail.com>
2026-07-29 12:30:32 +0400

    addition in AuthController to fix error (#9)

--- HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/Controllers/AuthController.java
+++ HealthcareEquipmentSystem/src/main/java/com/example/HealthcareEquipmentSystem/Controllers/AuthController.java
@@ -14,6 +14,7 @@ import java.util.Map;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
